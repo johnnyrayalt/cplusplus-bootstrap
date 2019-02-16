39465bad0b7e0ee6dff34095ae9c4725238b7b78
Johnny Ray Alt <johnnyrayalt@gmail.com>
2019-02-15 18:03:55 -0800

    interactive hello world

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string x;
+    std::cout << "enter your name" << std::endl;
+    std::cin >> x;
+    std::cout << "your name is: " << x << std::endl;
+
+    return 0;
+}
